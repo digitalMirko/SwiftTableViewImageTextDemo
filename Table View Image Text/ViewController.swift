caf27b8eb6357ac3a289f854dc20e2cb9b88bae7
Mirko Cukich <mirkocukich@gmail.com>
2019-08-24 23:03:29 -0500

    Initial Commit

--- Table View Image Text/ViewController.swift
+++ Table View Image Text/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mirko Cukich on 8/24/19.
 //  Copyright © 2019 Digital Mirko. All rights reserved.
 //
+//  Swift TableView Image & Text Demo - Demo 12 of 30
 
 import UIKit
 
@@ -12,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 

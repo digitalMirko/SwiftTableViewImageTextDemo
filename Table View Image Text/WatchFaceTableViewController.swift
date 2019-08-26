133554c2327194edf1d08ae29b258f06e3b8978c
Mirko Cukich <mirkocukich@gmail.com>
2019-08-25 19:05:01 -0500

    Program Complete

--- Table View Image Text/WatchFaceTableViewController.swift
+++ Table View Image Text/WatchFaceTableViewController.swift
@@ -7,86 +7,63 @@
 //
 //  Swift TableView Image & Text Demo - Demo 12 of 30
 
-
 import UIKit
 
-class WatchFaceTableViewController: UITableViewController {
+struct HeadLine {
+    
+    var id: Int
+    var title : String
+    var text : String
+    var image : String
+}
 
+class WatchFaceTableViewController: UITableViewController {
+    // Note: using 50x50 image graphics for row graphics - show up nicer then larger ones do since height is 60
+    var headlines = [
+        HeadLine(id: 1, title: "Rolex", text: "Blue Submariner", image: "01RolexBlue50"),
+        HeadLine(id: 2, title: "Tudor", text: "Petagos Blue", image: "02TudorPetagos50"),
+        HeadLine(id: 3, title: "Breitling", text: "Bentley Motors", image: "03BreitlingBentley50"),
+        HeadLine(id: 4, title: "Arnold & Son HM", text: "Double Hemisphere Perpetual Moon", image: "04ArnoldSonDoubleHemi50"),
+        HeadLine(id: 5, title: "Rolex", text: "Oyster Perpetual Air-King", image: "05RolexOysterPerp50"),
+        HeadLine(id: 6, title: "Rolex", text: "Oyster Perpetual Milgauss", image: "06RolexOyster50"),
+        HeadLine(id: 7, title: "Rolex", text: "Blue Submariner", image: "07RolexBlueSub50"),
+        HeadLine(id: 8, title: "Ferrari", text: "Yann Ferrari", image: "08FerrariYann50"),
+        HeadLine(id: 9, title: "Corum", text: "Admiral's Cup", image: "09CorumAdmiralsCup50"),
+        HeadLine(id: 10, title: "Omega", text: "Seamaster Professional", image: "10OmegaSeamasterPro50")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return headlines.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        let headline = headlines[indexPath.row]
+        cell.textLabel?.text = headline.title
+        cell.detailTextLabel?.text = headline.text
+        cell.imageView?.image = UIImage(named: headline.image)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return "Swift TableView Image & Text Demo"
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
